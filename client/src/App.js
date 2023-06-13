d6e78969445a91fd117c055739dc62f9aced479d
kvrp97 <ravinduprabhashwara97@gmail.com>
2023-06-13 08:36:21 +0530

    router re- configured

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 import Home from './components/Home'
 import CreatePost from './components/CreatePost'
 import EditPost from './components/EditPost'
@@ -8,16 +8,16 @@ import NavBar from './components/NavBar'
 
 export default class App extends Component {
   render() {
-    return (      
+    return (
+      <BrowserRouter>
         <div className='container'>
-          <NavBar/>
-          <Routes> 
-            <Route path='/' component={Home} />
-            <Route path='/add' component={CreatePost} />
-            <Route path='/edit/:id' component={EditPost} />
-            <Route path='/post/:id' component={PostDetails} />
-          </Routes>
-        </div>      
+          <NavBar />
+          <Route path='/' component={Home} />
+          <Route path='/add' component={CreatePost} />
+          <Route path='/edit/:id' component={EditPost} />
+          <Route path='/post/:id' component={PostDetails} />
+        </div>
+      </BrowserRouter>
     )
   }
 }
